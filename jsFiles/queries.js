6a8d5b094ed12c90a2a504f896d6c12f5da462e7
Matthew <40572723+marberrym@users.noreply.github.com>
2018-08-30 10:23:34 -0400

    Update queries.js

--- jsFiles/queries.js
+++ jsFiles/queries.js
@@ -1,5 +1,5 @@
 const pg = require('pg-promise')();
-const db = pg('postgres://marshallsimpson@localhost:5432/plunderdb')
+const db = pg('postgres://matt@localhost:5432/plunder')
 
 
 let listAllUsers = () => {
